7868b27ba60c74f4ec2ed85bb9f376d8d463d4a4
meikuto <kevinyuluo3@gmail.com>
2026-04-24 22:57:19 -0500

    cambio

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/user');
 });
 
 Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/create', [UserController::class, 'create']); 
-Route::get('/user/{id}', [UserController::class, 'show']);     
+Route::get('/user/create', [UserController::class, 'create']);
+Route::get('/user/{id}', [UserController::class, 'show']);
 Route::get('/user/{id}/edit', [UserController::class, 'edit']);
 Route::put('/user/{id}', [UserController::class, 'update']);
 Route::post('/user', [UserController::class, 'store']);
